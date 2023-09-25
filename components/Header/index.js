884e37eb1a4e9933f6094f46249a2c1319ae074e
Костя <Kostya@MacBook-Air.local>
2023-09-25 17:20:06 +0200

    fix-menu

--- components/Header/index.js
+++ components/Header/index.js
@@ -15,7 +15,7 @@ export default function Header() {
     const [isShow, setIsShow] = useState(false);
 
     React.useEffect(() => {
-        if (location.asPath === "/") {
+        if (location.asPath === "/" || location.asPath.startsWith("/?")) {
             setColorMenu(false);
         } else {
             setColorMenu(true);
